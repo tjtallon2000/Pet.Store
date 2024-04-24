5dfd57dca30a49fe9259cf4c01605f0c03385bc1
tjtallon2000 <157921458+tjtallon2000@users.noreply.github.com>
2024-04-23 22:03:19 -0600

    Add files via upload

---
+++ Pet-Store/src/main/java/Pet/Store/Entity/Employee.java
@@ -0,0 +1,33 @@
+package Pet.Store.Entity;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Employee {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long employeeId;
+	
+	private String employeeFirstName;
+	private String employeeLastName;
+	private String employeePhone;
+	private String employeeJobTitle;
+	
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pet_store_id", nullable = false)
+	private PetStore petStore;
+}
---
+++ Pet-Store/target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Customer (customerFirstName, customerLastName, customerEmail)
+VALUES ("Hope it works", "b", "h");
